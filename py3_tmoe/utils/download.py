8de7f15498e4eb97263e570c15e3328b6e152f2b
Legolas Wang <legolaswang0803@gmail.com>
2025-01-16 21:34:41 +0800

    feat:vivaldi finished

--- py3_tmoe/utils/download.py
+++ py3_tmoe/utils/download.py
@@ -18,7 +18,7 @@ def download(
 
     Params:
         str url: the remote file url
-        str file_path: the output file path
+        str file_path: the output file path, starts with a "/"
         bool no_conf: whether to use the default aria2 config file
         bool overwrite: whether to overwrite the already existed file
         bool check_cert: check certificate
@@ -26,16 +26,21 @@ def download(
     if not check_cmd_exists("aria2c"):
         raise CmdNotFoundError("aria2c")
 
+    ls_of_file_and_path: list[str] = file_path.split("/")
+    ls_of_file_and_path.pop(0)
+
     run(
         [
             "aria2c",
             "--console-log-level=info",
             "--no-conf" if no_conf else "",
-            *"-k 1M".split(" "),
+            *"-k 5M".split(" "),
             *"-s 5 -x 5".split(" "),
             f"--check-certificate={str(check_cert).lower()}",
             f"--allow-overwrite={str(overwrite).lower()}",
-            *f"-o {file_path}".split(" "),
+            "-o",
+            "/".join(ls_of_file_and_path),
+            *"-d /".split(" "),
             url,
         ],
         f"when downloading {url} to {file_path}",
