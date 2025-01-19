67a4b997dd505242ec43e87593562e1f7a593467
Legolas Wang <legolaswang0803@gmail.com>
2025-01-19 19:34:23 +0800

    change some var in firefox to private

--- py3_tmoe/apps/browser/firefox.py
+++ py3_tmoe/apps/browser/firefox.py
@@ -26,8 +26,7 @@ class Firefox:
         FirefoxVariants variant: the variant for firefox
     """
 
-    DISTRO: str = get_distro_short_name()[0]
-    OTHER_DISTRO: str = get_distro_short_name()[1]
+    _DISTRO, _OTHER_DISTRO = get_distro_short_name()
 
     def __init__(self, variant: FirefoxVariants) -> None:
         self.variant = variant
@@ -65,10 +64,10 @@ class Firefox:
         }
 
         # Use dict.get instead of if ... elif ... to avoid pylint warnings
-        self.dependency_main = dep_main_dict.get(self.DISTRO, "")
-        self.dependency_others = dep_others_dict.get(self.OTHER_DISTRO, [])
+        self.dependency_main = dep_main_dict.get(self._DISTRO, "")
+        self.dependency_others = dep_others_dict.get(self._OTHER_DISTRO, [])
 
-        if self.DISTRO == "gentoo":
+        if self._DISTRO == "gentoo":
             run(cmd_args=["dispatch-conf"], msg="when running dispatch-conf")
 
     def _set_ubuntu_firefox_priority(self) -> None:
@@ -84,9 +83,7 @@ class Firefox:
                 """Package: *\n
 Pin: release o=LP-PPA-mozillateam,l=Firefox ESR and Thunderbird stable builds
 Pin-Priority: 900
-""".split(
-                    "\n", maxsplit=1
-                )
+""".split("\n", maxsplit=1)
             )
             run(
                 ["chmod", "a+r", "-vf", "/etc/apt/preferences.d/90-mozilla-firefox"],
@@ -111,14 +108,14 @@ Pin-Priority: 900
         }
 
         # The same, avoid pylint warnings
-        self.dependency_main = dep_main_dict.get(self.DISTRO, "")
-        self.dependency_others = dep_others_dict.get(self.DISTRO, [])
+        self.dependency_main = dep_main_dict.get(self._DISTRO, "")
+        self.dependency_others = dep_others_dict.get(self._DISTRO, [])
 
         # Locales for ubuntu are different from other debian distros
-        if self.OTHER_DISTRO == "ubuntu":
+        if self._OTHER_DISTRO == "ubuntu":
             self.dependency_others = ["ffmpeg", "^firefox-locale-zh"]
 
-        if self.DISTRO == "gentoo":
+        if self._DISTRO == "gentoo":
             run(cmd_args=["dispatch-conf"], msg="when running dispatch-conf")
 
     def prepare(self) -> None:
@@ -127,7 +124,7 @@ Pin-Priority: 900
         """
 
         # Setup mozilla PPA and snap disable for ubuntu
-        if self.OTHER_DISTRO == "ubuntu":
+        if self._OTHER_DISTRO == "ubuntu":
             self._setup_ppa_env()
             run(
                 ["sudo", "add-apt-repository", "ppa:mozillateam/ppa", "-y"],
@@ -145,7 +142,7 @@ Pin-Priority: 900
         Installation for ESR
         """
         install_app(
-            distro=self.DISTRO, apps=[self.dependency_main, *self.dependency_others]
+            distro=self._DISTRO, apps=[self.dependency_main, *self.dependency_others]
         )
         if (not check_cmd_exists("firefox")) and (not check_cmd_exists("firefox-esr")):
             self._install_for_firefox()
@@ -156,7 +153,7 @@ Pin-Priority: 900
         """
         self._setup_ppa_env()
         install_app(
-            distro=self.DISTRO, apps=[self.dependency_main, *self.dependency_others]
+            distro=self._DISTRO, apps=[self.dependency_main, *self.dependency_others]
         )
         if not check_cmd_exists("firefox"):
             self._install_for_esr()
@@ -175,7 +172,7 @@ Pin-Priority: 900
         elif self.variant == FirefoxVariants.FIREFOX:
             self._install_for_firefox()
 
-        if self.DISTRO == "debian":
+        if self._DISTRO == "debian":
             postinst_file: str = (
                 "/var/lib/dpkg/info/"
                 + f"{'firefox' if self.variant == FirefoxVariants.FIREFOX else 'firefox-esr'}.postinst"
