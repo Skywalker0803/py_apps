e83c2006ce49e768b6848f545deb49bc25b58d8b
Legolas Wang <legolaswang0803@gmail.com>
2025-01-19 13:46:52 +0800

    add midori browser

---
+++ py3_tmoe/apps/browser/midori.py
@@ -0,0 +1,16 @@
+"""
+Install Midori open source browser
+"""
+
+from py3_tmoe.utils.utils import get_distro_short_name
+
+
+class Midori:
+    """
+    This the class for managing Midori installation
+    """
+
+    _DISTRO, _OTHER_DISTRO = get_distro_short_name()
+
+    def __init__(self) -> None:
+        pass
