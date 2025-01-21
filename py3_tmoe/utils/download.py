8f9a6b80a967ae9cbb68434bc709453c88881c12
Legolas Wang <legolaswang0803@gmail.com>
2025-01-21 20:34:40 +0800

    fmt

--- py3_tmoe/utils/download.py
+++ py3_tmoe/utils/download.py
@@ -2,6 +2,10 @@
 This module contains download functions for this proj
 """
 
+from json import loads
+
+from requests import get
+
 from py3_tmoe.errors.cmd_not_found import CmdNotFoundError
 from py3_tmoe.utils.utils import check_cmd_exists, run
 
@@ -45,3 +49,22 @@ def download(
         ],
         f"when downloading {url} to {file_path}",
     )
+
+
+def get_github_releases(repo: str, version: str = "latest"):
+    """
+    Get the GitHub releases file url list
+
+    Params:
+        repo: the repo path to be parsed, in the form of "RepoOwner/RepoName"
+        version: the version wanted, "latest" by default
+    """
+    json_content: dict = loads(
+        get(f"https://api.github.com/repo/{repo}/releases/{version}").text
+    )
+    assets: list[str] = []
+
+    for i in json_content["assets"]:
+        assets.append(i["browser_download_url"])
+
+    return assets
