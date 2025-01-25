168462bdcb38970c73665d80c32308e05949f56e
Legolas Wang <legolaswang0803@gmail.com>
2025-01-25 22:18:22 +0800

    add epiphany browser

---
+++ py3_tmoe/apps/browser/epiphany.py
@@ -0,0 +1,31 @@
+"""
+This is the module for Epiphany Browser (A.K.A. GNOME Web)
+"""
+
+from .common import Browser
+from py3_tmoe.utils.sys import check_architecture, get_distro_short_name
+
+
+class Epiphany(Browser):
+    """Epiphany (GNOME Web)"""
+
+    _ARCH: str = check_architecture()
+    _DISTRO, _OTHER_DISTRO = get_distro_short_name()
+
+    def __init__(self) -> None:
+        self.pkg: str = ""
+
+    def prepare(self) -> Browser:
+        match self._DISTRO:
+            case "debian":
+                self.pkg = "epiphany-browser"
+            case "redhat":
+                self.pkg = "epiphany"
+            case "arch":
+                self.pkg = "epiphany"
+            case "gentoo":
+                self.pkg = "www-client/epiphany"
+        return self
+
+    def install(self) -> Browser:
+        return self
