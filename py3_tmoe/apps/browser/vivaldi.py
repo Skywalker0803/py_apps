1427fb65b5a4546c5bc397b081d9b733f67a6224
Legolas Wang <legolaswang0803@gmail.com>
2025-01-26 18:37:13 +0800

    fmt

--- py3_tmoe/apps/browser/vivaldi.py
+++ py3_tmoe/apps/browser/vivaldi.py
@@ -10,10 +10,10 @@ from requests import get
 from py3_tmoe.apps.browser.common import Browser
 from py3_tmoe.errors.distro_x_only import DistroXOnlyError
 from py3_tmoe.errors.unsupported_arch import UnsupportedArchitectureError
+from py3_tmoe.utils.app_manage import install_app
 from py3_tmoe.utils.cmd import run
 from py3_tmoe.utils.network import download
 from py3_tmoe.utils.sys import check_architecture, get_distro_short_name
-from py3_tmoe.utils.app_manage import install_app
 
 
 class Vivaldi(Browser):
@@ -102,9 +102,11 @@ class Vivaldi(Browser):
 
         # Download the package from website except
         # when use_sys_pkg_manager is True
-        download(
-            url=self.pkg_url, file_path=file_path, overwrite=True
-        ) if self.use_sys_pkg_manager is False else None
+        (
+            download(url=self.pkg_url, file_path=file_path, overwrite=True)
+            if self.use_sys_pkg_manager is False
+            else None
+        )
 
         # For deb based distros
         if self._DISTRO == "debian":
