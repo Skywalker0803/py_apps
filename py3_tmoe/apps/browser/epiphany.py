9ba5e5441ae4430445da7d836757046c319fdd71
Legolas Wang <legolaswang0803@gmail.com>
2025-01-30 20:46:48 +0800

    fmt

--- py3_tmoe/apps/browser/epiphany.py
+++ py3_tmoe/apps/browser/epiphany.py
@@ -2,11 +2,10 @@
 This is the module for Epiphany Browser (A.K.A. GNOME Web)
 """
 
+from py3_tmoe.apps.browser.common import Browser
 from py3_tmoe.errors.distro_x_only import DistroXOnlyError
 from py3_tmoe.utils.app_manage import install_app
 from py3_tmoe.utils.sys import check_architecture, get_distro_short_name
-
-from py3_tmoe.apps.browser.common import Browser
 
 
 class Epiphany(Browser):
