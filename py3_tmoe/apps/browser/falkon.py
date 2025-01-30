e906c85e293499ee3f7ffe4067eb5f7db39e8398
Legolas Wang <legolaswang0803@gmail.com>
2025-01-30 19:26:20 +0800

    standardize browser common class extension

--- py3_tmoe/apps/browser/falkon.py
+++ py3_tmoe/apps/browser/falkon.py
@@ -7,3 +7,12 @@ from py3_tmoe.apps.browser.common import Browser
 
 class Falkon(Browser):
     """Falkon Browser"""
+
+    def __init__(self) -> None:
+        self.pkg: str = "falkon"
+
+    def prepare(self) -> Browser:
+        return self
+
+    def install(self) -> Browser:
+        return self
