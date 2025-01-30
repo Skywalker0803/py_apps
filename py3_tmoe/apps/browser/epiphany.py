7e6067b6410184814a53b37b4738638759133fc8
Legolas Wang <legolaswang0803@gmail.com>
2025-01-30 19:33:29 +0800

    standardize epiphany

--- py3_tmoe/apps/browser/epiphany.py
+++ py3_tmoe/apps/browser/epiphany.py
@@ -2,6 +2,7 @@
 This is the module for Epiphany Browser (A.K.A. GNOME Web)
 """
 
+from py3_tmoe.errors.distro_x_only import DistroXOnlyError
 from py3_tmoe.utils.app_manage import install_app
 from py3_tmoe.utils.sys import check_architecture, get_distro_short_name
 
@@ -16,17 +17,18 @@ class Epiphany(Browser):
 
     def __init__(self) -> None:
         self.pkg: str = ""
+        self._pkg_dict: dict[str, str] = {
+            "debian": "epiphany-browser",
+            "redhat": "epiphany",
+            "arch": "epiphany",
+            "gentoo": "www-client/epiphany",
+        }
 
     def prepare(self) -> Browser:
-        match self._DISTRO:
-            case "debian":
-                self.pkg = "epiphany-browser"
-            case "redhat":
-                self.pkg = "epiphany"
-            case "arch":
-                self.pkg = "epiphany"
-            case "gentoo":
-                self.pkg = "www-client/epiphany"
+        self.pkg = self._pkg_dict.get(self._DISTRO, "")
+
+        if self.pkg == "":
+            raise DistroXOnlyError(self._DISTRO, "Debian & RHEL & Archlinux & Gentoo")
         return self
 
     def install(self) -> Browser:
--- py3_tmoe/apps/browser/falkon.py
+++ py3_tmoe/apps/browser/falkon.py
@@ -3,15 +3,23 @@ Falkon Browser
 """
 
 from py3_tmoe.apps.browser.common import Browser
+from py3_tmoe.utils.sys import check_architecture, get_distro_short_name
 
 
 class Falkon(Browser):
     """Falkon Browser"""
 
+    _ARCH: str = check_architecture()
+    _DISTRO, _OTHER_DISTRO = get_distro_short_name()
+
     def __init__(self) -> None:
-        self.pkg: str = "falkon"
+        self.pkg: str = ""
 
     def prepare(self) -> Browser:
+        match self._DISTRO:
+            case "debian":
+                self.pkg = "falkon"
+
         return self
 
     def install(self) -> Browser:
