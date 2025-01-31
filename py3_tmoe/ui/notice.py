e7f50af4a4ad48d38c7398cc6b702b42db1cddc8
Legolas Wang <legolaswang0803@gmail.com>
2025-01-31 19:45:40 +0800

    add ui notice

---
+++ py3_tmoe/ui/notice.py
@@ -0,0 +1,12 @@
+"""
+A "confirm-only" notice dialog
+"""
+
+from textual.app import App, ComposeResult
+
+from textual.widgets import Label
+
+
+class Notice(App[str]):
+    def compose(self) -> ComposeResult:
+        yield Label("ss")
