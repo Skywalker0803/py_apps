b2a4e09413c678f1d323b9952ce8dc36c727c9ab
Legolas Wang <legolaswang0803@gmail.com>
2025-02-01 21:53:37 +0800

    add ui notice

--- py3_tmoe/ui/notice.py
+++ py3_tmoe/ui/notice.py
@@ -4,11 +4,28 @@ A "confirm-only" notice dialog
 
 from textual.app import App, ComposeResult
 
-from textual.widgets import Label
+from textual.widgets import Button, Label
 
 
 class Notice(App[str]):
-    """A "confirm-only" notice dialog"""
+    """
+    A "confirm-only" notice dialog
+
+    Params:
+        str msg: the notice message
+        str ok: the ok button content
+    """
+
+    CSS_PATH = "notice.tcss"
+
+    def __init__(self, msg: str, ok: str = "OK"):
+        super().__init__()
+        self.msg = msg
+        self.ok = ok
 
     def compose(self) -> ComposeResult:
-        yield Label("ss")
+        yield Label(":warning: 注意")
+        yield Button(self.ok, variant="success", id="ok")
+
+    def on_button_pressed(self, event: Button.Pressed):
+        self.exit(event.button.id)
