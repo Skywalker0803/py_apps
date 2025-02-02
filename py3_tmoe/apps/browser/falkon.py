a4fc34b4522b45395305126dab18ed5a78b4b1d0
Legolas Wang <legolaswang0803@gmail.com>
2025-02-02 12:30:06 +0800

    add falkon sandbox bin

--- py3_tmoe/apps/browser/falkon.py
+++ py3_tmoe/apps/browser/falkon.py
@@ -6,6 +6,7 @@ from py3_tmoe.apps.browser.common import Browser
 from py3_tmoe.errors.distro_x_only import DistroXOnlyError
 from py3_tmoe.utils.app_manage import install_app
 from py3_tmoe.utils.sys import check_architecture, get_distro_short_name
+from py3_tmoe.utils.cmd import run
 
 from py3_tmoe.ui.notice import Notice
 
@@ -38,6 +39,35 @@ class Falkon(Browser):
 
     def install(self) -> Browser:
         install_app(self._DISTRO, [self.pkg])
+
+        with open(
+            "/usr/local/bin/falkon-no-sandbox", mode="+w", encoding="utf-8"
+        ) as falkon_no_sandbox:
+            falkon_no_sandbox.writelines(
+                [
+                    "#!/usr/bin/env bash",
+                    "###############",
+                    "if [[ $(command -v falkon) ]]; then",
+                    "   TMOE_BIN='falkon'",
+                    "elif [[ $(command -v falkon-browser) ]]; then",
+                    "   TMOE_BIN='falkon-browser'",
+                    "elif [[ $(command -v org.kde.falkon) ]]; then",
+                    "   TMOE_BIN='org.kde.falkon'",
+                    "fi",
+                    "",
+                    'case "$(id -u)" in',
+                    '0) exec ${TMOE_BIN} --no-sandbox "$@" ;;',
+                    "*)",
+                    '   ${TMOE_BIN} ${ADDITIONAL} "$@"',
+                    '   case "$?" in',
+                    "   0) ;;",
+                    '   *) exec ${TMOE_BIN} --no-sandbox "$@" ;;',
+                    "   esac",
+                    "   ;;",
+                    "esac",
+                ]
+            )
+        run(["chmod", "+x", "-vf", "/usr/bin/falkon-no-sandbox"])
 
         notice = Notice("若不能使用Falkon，请启动falkon-no-sandbox").run()
         assert notice == "ok"
