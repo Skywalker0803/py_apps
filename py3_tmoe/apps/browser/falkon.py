ef6604327dcbcd178ebbc6b4f8f5513a157b3cb0
Legolas Wang <legolaswang0803@gmail.com>
2025-02-02 12:34:29 +0800

    fmt

--- py3_tmoe/apps/browser/falkon.py
+++ py3_tmoe/apps/browser/falkon.py
@@ -4,11 +4,10 @@ Falkon Browser
 
 from py3_tmoe.apps.browser.common import Browser
 from py3_tmoe.errors.distro_x_only import DistroXOnlyError
-from py3_tmoe.utils.app_manage import install_app
-from py3_tmoe.utils.sys import check_architecture, get_distro_short_name
-from py3_tmoe.utils.cmd import run
-
 from py3_tmoe.ui.notice import Notice
+from py3_tmoe.utils.app_manage import install_app
+from py3_tmoe.utils.cmd import run
+from py3_tmoe.utils.sys import check_architecture, get_distro_short_name
 
 
 class Falkon(Browser):
