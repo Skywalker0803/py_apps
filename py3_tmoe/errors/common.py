a65af55b47a7e6c71d3fb7782cd2c363af77363c
Legolas Wang <legolaswang0803@gmail.com>
2025-02-05 21:11:51 +0800

    rm py3_tmoe

--- py3_tmoe/errors/common.py
+++
@@ -1,5 +0,0 @@
-"""
-This module defines some common vars for error package
-"""
-
-universal_msg: str = "\033[91m\033[1m[Error]\033[0m"
--- py3_tmoe/main.py
+++
@@ -1,8 +0,0 @@
-"""
-项目的main模块
-"""
-
-from py3_tmoe.pages.main import run
-
-
-run()
