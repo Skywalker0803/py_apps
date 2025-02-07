9aeb33785d09a5d5fd5c3ac042f80241d5472996
Legolas Wang <legolaswang0803@gmail.com>
2025-02-07 20:48:07 +0800

    add basic prepare() for nvim

--- py_apps/apps/devtools/neovim.py
+++ py_apps/apps/devtools/neovim.py
@@ -4,6 +4,8 @@ Neovim config & setup class
 
 from enum import Enum, unique
 
+from py_apps.utils.sys import check_architecture, get_distro_short_name
+
 
 @unique
 class NvimVariants(Enum):
@@ -15,3 +17,26 @@ class NvimVariants(Enum):
     SPACE = "space"
     NVCHAD = "nvchad"
     DEFAULT = "default"
+
+
+class Neovim:
+    """
+    Neovim config & setup
+    """
+
+    _ARCH = check_architecture()
+    _DISTRO, _OTHER_DISTRO = get_distro_short_name()
+
+    def __init__(self, variant: NvimVariants) -> None:
+        use_sys_pkg_dict: dict[str, bool] = {
+            "debian_arm64": False,
+            "debian_amd64": False,
+        }
+        self.variant = variant
+        self.use_sys_pkg: bool = use_sys_pkg_dict.get(
+            f"{self._DISTRO}_{self._ARCH}", True
+        )
+
+    def prepare(self):
+        self.pkg = "neovim"
+        return self
