bd44f6467a0922887da5fe8271f256afa43d6159
Legolas Wang <legolaswang0803@gmail.com>
2025-02-13 08:02:34 +0800

    add jb vars

--- py_apps/apps/devtools/jetbrains.py
+++ py_apps/apps/devtools/jetbrains.py
@@ -5,12 +5,16 @@ from enum import Enum, unique
 
 @unique
 class JetbrainsVarianta(Enum):
-    IDEA_COMMUNITY = "idea_community"
-    IDEA_PRO = "idea_pro"
+    IDEA_COMMUNITY = "intellij-idea_community"
+    IDEA_PRO = "intellij-idea_professional"
     PYCHARM_COMMUNITY = "pycharm_community"
     PYCHARM_PRO = "pycharm_professional"
     GOLAND = "goland"
     PHPSTORM = "phpstorm"
+    CLION = "clion"
+    RIDER = "rider"
+    RUSTROVER = "rustrover"
+    RUBYMINE = "rubymine"
 
 
 class Jetbrains:
