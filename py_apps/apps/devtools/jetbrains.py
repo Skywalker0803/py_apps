206bfd85c917d778dc1b3e60b4d0c4b7c75946dc
Legolas Wang <legolaswang0803@gmail.com>
2025-02-13 21:56:40 +0800

    bug:jb js not loading

--- py_apps/apps/devtools/jetbrains.py
+++ py_apps/apps/devtools/jetbrains.py
@@ -3,10 +3,15 @@
 from enum import Enum, unique
 
 
+from bs4 import BeautifulSoup
+
+from py_apps.utils.network import get
+
+
 @unique
-class JetbrainsVarianta(Enum):
-    IDEA_COMMUNITY = "intellij-idea_community"
-    IDEA_PRO = "intellij-idea_professional"
+class JetbrainsVariants(Enum):
+    IDEA_COMMUNITY = "idea_community"
+    IDEA_PRO = "idea_professional"
     PYCHARM_COMMUNITY = "pycharm_community"
     PYCHARM_PRO = "pycharm_professional"
     GOLAND = "goland"
@@ -20,5 +25,38 @@ class JetbrainsVarianta(Enum):
 class Jetbrains:
     """Jetbrains IDE Family Classes"""
 
-    def __init__(self) -> None:
-        self.page: str = ""
+    def __init__(self, variant: JetbrainsVariants) -> None:
+        self.variant = variant
+        self.product: str = variant.value.split("_")[0]
+        self.edition: str | None = (
+            variant.value.split("_")[-1]
+            if self.product != self.variant.value.split("_")[-1]
+            else None
+        )
+
+        self.page: str = f"https://www.jetbrains.com/{self.product}/download"
+
+        self.link = ""
+
+    def prepare(self):
+        soup: BeautifulSoup = BeautifulSoup(get(self.page).text, "html.parser")
+        links = soup.find_all(
+            "a",
+            {
+                # "class": "linux-download-link",
+                # "data-product": self.product,
+                # "data-type": self.edition,
+            },
+        )
+
+        for element in links:
+            self.link = element["href"] if links else None
+
+        if self.link is None:
+            raise Exception("Bug in Jetbrains get download link")
+
+        print(links)
+        print(type(links))
+        print(self.link)
+
+        return self
--- py_apps/main.py
+++ py_apps/main.py
@@ -2,7 +2,9 @@
 项目的main模块
 """
 
+from py_apps.apps.devtools.jetbrains import Jetbrains, JetbrainsVariants
 from py_apps.pages.main import run
 
+Jetbrains(JetbrainsVariants.IDEA_COMMUNITY).prepare()
 
 run()
