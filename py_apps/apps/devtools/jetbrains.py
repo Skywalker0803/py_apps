e22060a8965edea1052e718220efa426d248fdce
Legolas Wang <legolaswang0803@gmail.com>
2025-02-17 18:37:06 +0800

    add some ide

--- py_apps/apps/devtools/jetbrains.py
+++ py_apps/apps/devtools/jetbrains.py
@@ -8,6 +8,12 @@ from py_apps.utils.sys import check_architecture
 
 jetbrains_ver: dict[str, str] = {
     "idea": "2024.3.2.1",
+    "python": "2024.3.3",
+    "go": "2024.3.3",
+    "webide": "2024.3.3",
+    "webstorm": "2024.3.2.1",
+    "cpp": "2024.3.3",
+    "rider": "2024.3.5",
 }
 
 
@@ -15,11 +21,11 @@ jetbrains_ver: dict[str, str] = {
 class JetbrainsVariants(Enum):
     IDEA_COMMUNITY = "idea_community"
     IDEA_PRO = "idea_professional"
-    PYCHARM_COMMUNITY = "pycharm_community"
-    PYCHARM_PRO = "pycharm_professional"
-    GOLAND = "goland"
-    PHPSTORM = "phpstorm"
-    CLION = "clion"
+    PYCHARM_COMMUNITY = "python_community"
+    PYCHARM_PRO = "python_professional"
+    GOLAND = "go"
+    PHPSTORM = "webide"
+    CLION = "cpp"
     RIDER = "rider"
     RUSTROVER = "rustrover"
     RUBYMINE = "rubymine"
@@ -33,6 +39,7 @@ class Jetbrains:
     def __init__(self, variant: JetbrainsVariants) -> None:
         self.variant = variant
         self.product: str = variant.value.split("_")[0]
+
         self.edition: str | None = (
             variant.value.split("_")[-1]
             if self.product != self.variant.value.split("_")[-1]
@@ -47,9 +54,21 @@ class Jetbrains:
         file_name = {
             "idea_community": "ideaIC",
             "idea_professional": "ideaU",
+            "python_community": "pycharm-community",
+            "python_professional": "pycharm-professional",
+            "go": "goland",
+            "webide": "PhpStorm",
+            "webstorm": "WebStorm",
+            "cpp": "CLion",
+            "rider": "JetBrains.Rider",
         }[self.variant.value]
 
-        self.link = f"https://download.jetbrains.com/{self.product}/{file_name}-{jetbrains_ver[self.product]}{'-aarch64' if self._ARCH=='arm64' else ''}.tar.gz"
+        self.link = (
+            f"https://download.jetbrains.com/{self.product}/"
+            + f"{file_name}-{jetbrains_ver[self.product]}"
+            + ("-aarch64" if self._ARCH == "arm64" else "")
+            + ".tar.gz"
+        )
 
         if self.link is None:
             raise Exception("Bug in Jetbrains get download link")
