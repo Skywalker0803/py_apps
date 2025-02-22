a516b5ad54073c1613d906feab78f02af6d07be0
Legolas Wang <legolaswang0803@gmail.com>
2025-02-22 20:12:55 +0800

    ??

--- py_apps/pages/browser.py
+++ py_apps/pages/browser.py
@@ -4,22 +4,22 @@ Index for browser page
 
 from sys import exit as sys_exit
 
-from py_apps.apps.browser.epiphany import Epiphany as _Epiphany
+from py_apps.apps.browser.epiphany import Epiphany
 from py_apps.apps.browser.falkon import Falkon
-from py_apps.apps.browser.firefox import Firefox as _Firefox
-from py_apps.apps.browser.firefox import FirefoxVariants as _FirefoxVariants
-from py_apps.apps.browser.midori import Midori as _Midori
-from py_apps.apps.browser.vivaldi import Vivaldi as _Vivaldi
-from py_apps.errors.distro_x_only import DistroXOnlyError as _DistroXOnlyError
-from py_apps.ui.dialog import Dialog as _Dialog
-from py_apps.ui.selection import Selection as _Selection
+from py_apps.apps.browser.firefox import Firefox
+from py_apps.apps.browser.firefox import FirefoxVariants
+from py_apps.apps.browser.midori import Midori
+from py_apps.apps.browser.vivaldi import Vivaldi
+from py_apps.errors.distro_x_only import DistroXOnlyError
+from py_apps.ui.dialog import Dialog
+from py_apps.ui.selection import Selection
 
 
 def run() -> None:
     """
     Run browser selection page
     """
-    selection = _Selection(
+    selection = Selection(
         idlist=["firefox", "vivaldi", "midori", "epiphany", "falkon"],
         itemlist=[
             ":fox_face: Firefox 浏览器：为自由而生",
@@ -34,36 +34,36 @@ def run() -> None:
 
     match result:
         case "firefox":
-            choose: str | None = _Dialog(
+            choose: str | None = Dialog(
                 idlist=["firefox", "esr"],
                 itemlist=["Firefox 火狐浏览器", "Firefox ESR 长期支持版"],
                 dialog_title="Firefox 还是 ESR ？",
             ).run()
 
-            opt: _FirefoxVariants = _FirefoxVariants.ESR
+            opt: FirefoxVariants = FirefoxVariants.ESR
 
             if choose == "firefox":
-                opt = _FirefoxVariants.FIREFOX
+                opt = FirefoxVariants.FIREFOX
 
             elif choose == "esr":
-                opt = _FirefoxVariants.ESR
+                opt = FirefoxVariants.ESR
 
             else:
                 print(f"BUG in {__package__}")
                 sys_exit(f"BUG in {__package__}")
 
-            _Firefox(variant=opt).prepare().install()
+            Firefox(variant=opt).prepare().install()
         case "vivaldi":
             try:
-                _Vivaldi().prepare().install()
-            except _DistroXOnlyError as err:
+                Vivaldi().prepare().install()
+            except DistroXOnlyError as err:
                 print(str(err))
                 sys_exit("distro x only err")
         case "midori":
-            _Midori().prepare().install()
+            Midori().prepare().install()
 
         case "epiphany":
-            _Epiphany().prepare().install()
+            Epiphany().prepare().install()
         case "falkon":
             Falkon().prepare().install()
         case _:
